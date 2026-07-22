2317025955afdb89469416538c0dfe32a9c82ead
loderx <53370865+loderx@users.noreply.github.com>
2026-07-22 19:12:30 +0300

    Place plugin bootstrap at archive root

---
+++ lod-woo-ajax-filters.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: LOD Woo AJAX Filters
+ * Description: Lightweight WooCommerce AJAX filters for price and product attributes.
+ * Version: 1.0.2
+ * Author: LOD
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * WC requires at least: 7.0
+ */
+if (!defined('ABSPATH')) exit;
+require_once __DIR__ . '/lod-woo-ajax-filters/lod-woo-ajax-filters.php';
